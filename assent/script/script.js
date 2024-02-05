1422bc1b6e3fd606e0cca1573061bffcfdd6a52e
ZIAD AHMED <104504486+ziadAhmedFawzy@users.noreply.github.com>
2024-02-05 05:34:19 +0200

    finish

---
+++ assent/script/script.js
@@ -0,0 +1,9 @@
+let btnScrolling = document.getElementById("btnScrolling")
+
+btnScrolling.onclick = () => {
+    window.scroll({
+        left: 0,
+        top:10188.5712890625,
+        behavior: "smooth"
+    })
+}
